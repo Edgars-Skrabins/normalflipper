70b2d57d72ae1b5f690b831ede153034a92b6926
Edgars Skrabiņš <shiftkiro@gmail.com>
2023-09-01 11:30:42 +0300

    Project setup

---
+++ TextureProcessor.js
@@ -0,0 +1,43 @@
+﻿const dropArea = document.getElementById('drop-area');
+const fileInput = document.getElementById('file-input');
+const preview = document.getElementById('preview');
+
+dropArea.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    dropArea.classList.add('active');
+});
+
+dropArea.addEventListener('dragleave', () => {
+    dropArea.classList.remove('active');
+});
+
+dropArea.addEventListener('drop', (event) => {
+    event.preventDefault();
+    dropArea.classList.remove('active');
+    handleFiles(event.dataTransfer.files);
+});
+
+fileInput.addEventListener('change', () => {
+    handleFiles(fileInput.files);
+});
+
+
+function handleFiles(files) {
+
+    preview.innerHTML = '';
+    
+    for (const file of files) {
+        if (file.type.match(/^image\/(jpeg|jpg|png)$/)) {
+            const reader = new FileReader();
+
+            reader.onload = (event) => {
+                const image = new Image();
+                image.src = event.target.result;
+                image.classList.add("previewImage");
+                preview.appendChild(image);
+            };
+
+            reader.readAsDataURL(file);
+        }
+    }
+}
