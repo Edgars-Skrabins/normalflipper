d45898ef5e2a34028b7633d468c1a738ba02542f
Edgars Skrabiņš <shiftkiro@gmail.com>
2023-09-01 12:37:07 +0300

    Normal flipping logic and buttons

--- TextureProcessor.js
+++ TextureProcessor.js
@@ -21,23 +21,80 @@ fileInput.addEventListener('change', () => {
     handleFiles(fileInput.files);
 });
 
+let flipButton = document.getElementById("flip-button");
+
+flipButton.addEventListener("click", flipNormalMap);
+
+function flipNormalMap()
+{
+    const imageElement = preview.firstChild;
+    
+    if (imageElement) {
+        const normalMapImage = new Image();
+        normalMapImage.src = imageElement.getAttribute('src');
+
+        preview.removeChild(imageElement);
+
+        const convertedImage = convertNormalMap(normalMapImage, 'OpenGLToDirectX');
+
+        convertedImage.classList.add("previewImage");
+        preview.appendChild(convertedImage);
+        
+    }
+}
 
 function handleFiles(files) {
-
-    preview.innerHTML = '';
     
+    if(preview.firstChild) preview.removeChild(preview.firstChild);
+
     for (const file of files) {
-        if (file.type.match(/^image\/(jpeg|jpg|png)$/)) {
-            const reader = new FileReader();
+        
+        if (!file.type.match(/^image\/(jpeg|jpg|png)$/)) return;
+        
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const image = new Image();
+            image.src = event.target.result;
+            image.classList.add("previewImage");
 
-            reader.onload = (event) => {
-                const image = new Image();
-                image.src = event.target.result;
-                image.classList.add("previewImage");
-                preview.appendChild(image);
-            };
+            preview.appendChild(image);
+        };
+        reader.readAsDataURL(file);
+    }
+}
 
-            reader.readAsDataURL(file);
+function convertNormalMap(normalMapImage, conversionDirection) {
+    
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = normalMapImage.width;
+    canvas.height = normalMapImage.height;
+
+    ctx.drawImage(normalMapImage, 0, 0);
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const pixels = imageData.data;
+
+    if (conversionDirection === 'OpenGLToDirectX') {
+        for (let y = 0; y < canvas.height; y++) {
+            for (let x = 0; x < canvas.width; x++) {
+                const i = (y * canvas.width + x) * 4;
+                pixels[i + 1] = 255 - pixels[i + 1];
+            }
+        }
+    } else if (conversionDirection === 'DirectXToOpenGL') {
+        for (let y = 0; y < canvas.height; y++) {
+            for (let x = 0; x < canvas.width; x++) {
+                const i = (y * canvas.width + x) * 4;
+                pixels[i + 1] = 255 - pixels[i + 1];
+            }
         }
     }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    const modifiedImage = new Image();
+    modifiedImage.src = canvas.toDataURL();
+
+    return modifiedImage;
 }
